2a1928383dc309f274779c5e7da7ae2005849504
zappz88 <31833538+zappz88@users.noreply.github.com>
2020-01-13 12:17:08 -0600

    Initials Files

---
+++ Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+	<%-- This is where page customizations go. --%>
+	<title>Query Dashboard</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Main" Runat="Server">
+	<div id="MainContainer" runat="server">
+		<div id="Main" runat="server">
+		</div>
+	</div>
+    <script>
+		window.onload = () => {
+
+            let main = document.getElementById('<%=Main.ClientID%>');
+            main.classList.add('main');
+
+            let displayContainer = document.createElement('div');
+            displayContainer.classList.add('displayContainer');
+            displayContainer.id = 'displayContainer';
+
+            main.appendChild(displayContainer);
+
+            let getAllQueries = JSON.parse(JSON.parse(loadAllQueries()));
+            console.log(getAllQueries);
+
+			getAllQueries.forEach(item => {
+                if (item !== null) {
+					displayContainer.append(createDefaultWidget(item));
+				}
+				else {
+					console.log(item);
+				}
+			})
+
+        };
+    </script>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Web.Script.Services;
+using System.Web.Security;
+using System.Web.Services;
+
+public partial class _Default : System.Web.UI.Page
+{
+	[WebMethod]
+	[ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+	public static string GetQueries()
+	{
+		var data = new Data();
+		var loadQueries = data.LoadQueries();
+		return JsonConvert.SerializeObject(loadQueries);
+	}
+
+	[WebMethod]
+	[ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+	public static string DataSwitch(string QueryObject)
+	{
+		try
+		{
+			QueryObjectModel queryObjectModel = JsonConvert.DeserializeObject<QueryObjectModel>(QueryObject);
+			QueryResultModel queryResultModel = new QueryResultModel();
+
+			string query = queryObjectModel.QueryText;
+
+			if (queryObjectModel.InputParameters.Length > 0 && queryObjectModel.InputsArray.Length > 0)
+			{
+				query = StringUtils.ReplaceAll(queryObjectModel.QueryText, queryObjectModel.InputParameters, queryObjectModel.InputsArray, queryObjectModel.DataTypesArray);
+			}
+
+			Data data = new Data();
+			var value = (DataTable)data.GetTableData(queryObjectModel.ServerName, queryObjectModel.DatabaseName, query);
+			queryResultModel.Result = JsonConvert.SerializeObject(value);
+			queryResultModel.ResultType = queryObjectModel.ResultType;
+			queryResultModel.Caption = queryObjectModel.Caption;
+			return JsonConvert.SerializeObject(queryResultModel);
+		}
+		catch (Exception ex)
+		{
+			return null;
+		}
+	}
+}
